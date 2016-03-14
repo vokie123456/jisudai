4f8197f2ffaa3f06413a6012fc269d231133bc4c
周勇 <zhouyong@haodai.com>
2016-03-14 12:29:29 +0800

    Initial Commit

---
+++ jisudai/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jisudai
+//
+//  Created by haodai on 16/3/14.
+//  Copyright © 2016年 xiaoya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ jisudai/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  jisudai
+//
+//  Created by haodai on 16/3/14.
+//  Copyright © 2016年 xiaoya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
